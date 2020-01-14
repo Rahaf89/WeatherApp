a8cfa025c8ce43d6d3cef9fc45d7d00697f7222b
rahaf89 <rahaflh.900@hotmail.com>
2020-01-14 14:36:47 -0500

    message

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 // api key : 82005d27a116c2880c8f0fcb866998a0
+let latitude;
+let longitude;
+const notification = document.getElementsByClassName("notification")[0];
 
+getLocation();
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition, showError);
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
   }
 }
 
-function showPosition(position) {
+function onSuccess(position) {
   console.log(position);
 }
 
-function showError(error) {
-  console.log(error);
+function onError(error) {
+  console.error("No no no", error);
+
+  const p = document.createElement("p");
+  p.innerHTML = error.message;
+  notification.style.display = "block";
+  notification.appendChild(p);
 }
 
 getLocation();
