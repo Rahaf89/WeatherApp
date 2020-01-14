c515d823972f9ad94c5eb2e85bca14399652b026
rahaf89 <rahaflh.900@hotmail.com>
2020-01-14 15:12:39 -0500

    promise

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-// api key : 82005d27a116c2880c8f0fcb866998a0
+// api key : a70238d9b9a0b059a30d04828b67a9d7
+//https://samples.openweathermap.org/data/2.5/weather?
+//lat=35
+//&lon=139
+//&appid=b6907d289e10d714a6e88b30761fae22
+
+const apiKey = "a70238d9b9a0b059a30d04828b67a9d7";
 let latitude;
 let longitude;
 const notification = document.getElementsByClassName("notification")[0];
@@ -12,6 +18,18 @@ function getLocation() {
 
 function onSuccess(position) {
   console.log(position);
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  const weatherPromise = fetch(
+    "https://api.openweathermap.org/data/2.5/weather?" +
+      "lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
+      "&appid=" +
+      apiKey
+  );
+  weatherPromise.then(x => console.log(x.body));
 }
 
 function onError(error) {
