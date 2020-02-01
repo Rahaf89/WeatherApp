75397caf380181403051db7e07b122ec6e315bdc
Rahaf89 <56045422+Rahaf89@users.noreply.github.com>
2020-02-01 15:05:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,71 +4,62 @@
 //&lon=139
 //&appid=b6907d289e10d714a6e88b30761fae22
 
-const apiKey = "a70238d9b9a0b059a30d04828b67a9d7";
-let latitude;
+const apikey = "a70238d9b9a0b059a30d04828b67a9d7";
+
 let longitude;
-const notification = document.getElementsByClassName("notification")[0];
+let temperatureValue = document.querySelector('.temperature-value p');
+const notification = document.getElementsByClassName('notification')[0];
+let weatherIcon = document.querySelector('.weather-icon');
+let locationElement = document.querySelector('.location p');
+let temperatureDescription = document.querySelector('.temperature-description p');
 
 function getLocation() {
-  //console.log(navigator.geolocation);
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-  } else {
-    notification.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-function kelvinToCelsius(temp) {
-  return temp - 273.15;
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	}
 }
 
 function onSuccess(position) {
-  console.log(position);
-  latitude = position.coords.latitude;
-  longitude = position.coords.longitude;
-  const weatherPromise = fetch(
-    "https://api.openweathermap.org/data/2.5/weather?" +
-      "lat=" +
-      latitude +
-      "&lon=" +
-      longitude +
-      "&appid=" +
-      apiKey
-  );
-  weather
-    .then((response) => response.json())
-    .then((weather) => {
-      //console.log(weather);
-      //console.log(weather.weather[0].icon);
-      //console.log(parseInt(weather.main.temp - 273.15));
-      //console.log(weather.weather[0].main);
-      //console.log(weather.name);
+	//console.log(position);
+	latitude = position.coords.latitude;
+	longitude = position.coords.longitude;
 
-      const location = weather.name;
-      const loacationHtmlElement = document.getElementsByClassName("location")[0];
-      loacationHtmlElement.innerHTML = "<p>" + location + "</p>";
-      const temp = kelvinToCelsius(weather.main.temp).toFixed(1);
-      const tempHtmlElement = document.getElementsByClassName("temperature-value")[0];
-      tempHtmlElement.innerHTML = "<p>" + temp + "°<span>C</span></p>";
-      const icon = weather.weather[0].icon;
-      const iconHtmlElement = document.getElementsByClassName("weather-icon")[0];
-      iconHtmlElement.innerHTML = "<img src='icons/" + icon + ".png' alt=''></img>";
-      const weatherDescription = weather.weather[0].main;
-      const weatherDescriptionHtmlElement = document.getElementsByClassName(
-        "temperature-description"
-      )[0];
-      weatherDescriptionHtmlElement.innerHTML = "<p>" + weatherDescription + "</p>";
-    });
+	const weather = fetch(
+		'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + apikey
+	);
+
+	weather
+		.then(response => response.json())
+		.then(weatherInfo => {
+			//console.log(temperatureValue);
+			temperatureValue.innerHTML = `${parseInt(weatherInfo.main.temp - 273.15)} °<span>C</span>`;
+
+			//console.log(`<img src="icons/${weatherInfo.weather[0].icon}.png"`);
+			weatherIcon.innerHTML = `<img src="icons/${weatherInfo.weather[0].icon}.png"/>`;
+
+			//console.log(weatherInfo.name);
+			//console.log(locationElement)
+			locationElement.innerHTML = `${weatherInfo.name + ' ' + weatherInfo.sys.country}`;
+
+			temperatureDescription.innerHTML = `${weatherInfo.weather[0].description}`;
+			//console.log(weatherInfo.weather[0].main);
+
+			//console.log(parseInt(weatherInfo.main.temp - 273.15));
+			console.log(`Thanks to check my code '-'`);
+		});
 }
-//console.log("info", weather);
-//console.log(position);
-//latitude=position.coords.
 
 function onError(error) {
-  console.error(error);
-  //notification[0].innerHTML = error.message;
-  const p = document.createElement("p");
-  p.innerHTML = error.message;
-  notification.style.display = "block";
-  notification.appendChild(p);
+	//console.log('NO no no ', error);
+
+	//notification[0].innerText.message;
+	const p = document.createElement('p');
+	p.innerHTML = error.message;
+
+	notification.style.display = 'block';
+
+	notification.appendChild(p);
 }
+
 getLocation();
+
